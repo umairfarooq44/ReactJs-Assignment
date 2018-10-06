3a7efd166866b51b182f53563223ee1a98579626
Umair Farooq <umairfarooq44@gmail.com>
2018-10-06 17:52:57 +0500

    Added webpack html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var config = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js',
+        filename: '[name].[chunkhash].js',
         publicPath: '/'
     },
     module: {
@@ -19,7 +20,15 @@ var config = {
                 loaders: ['style-loader', 'css-loader', 'sass-loader']
               },
         ]
-    }
+    },
+    plugins: [ 
+        new HtmlWebpackPlugin({
+          inject: false,
+          hash: true,
+          template: './index.html',
+          filename: 'index.html'
+        }),
+      ]
 };
 
 module.exports = config;
